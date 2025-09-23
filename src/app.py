8d22bf591a8a9e444f9264064cd318a416c9d272
Tyrel Orde Fecha <tyrelorde.fecha@gmail.com>
2025-09-23 11:43:51 +0800

    Add deploy message

--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ def details():
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
         'message': 'You are doing great, human!!! XOXO'
+        'deployed_on': 'kubernetes'
     })
 
 @app.route('/api/v1/healthz')
